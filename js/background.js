13b7d37509e53eb1088bb50ddc81f8a402540464
stilinskii <lhy1031hy@gmail.com>
2022-05-28 02:16:58 +0900

    add images

--- js/background.js
+++ js/background.js
@@ -1,4 +1,13 @@
-const images = ['0.jpg', '1.jpg', '2.jpg', '3.jpg', '4.jfif', '5.jfif'];
+const images = [
+  '0.jpg',
+  '1.jpg',
+  '2.jpg',
+  '3.jpg',
+  '4.jfif',
+  '5.jfif',
+  '6.jpg',
+  '7.jfif',
+];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 console.log(chosenImage);
 
